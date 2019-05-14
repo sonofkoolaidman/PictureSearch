63ce5417668f83bd52d5ff51a9f440887a3d9d65
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-05-14 09:01:06 -0500

    added more work

--- app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
+++ app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
@@ -1,6 +1,14 @@
 package com.ctech.max.picturesearch;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class PictureFragment extends Fragment {
+
+    private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
+
+    
 }
