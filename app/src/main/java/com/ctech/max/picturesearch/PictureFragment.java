6695ea10ec4ba66d19815164ded5220f330e690a
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-05-15 08:36:27 -0500

    connected java and layout.

--- app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
+++ app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
@@ -1,7 +1,12 @@
 package com.ctech.max.picturesearch;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -10,5 +15,18 @@ public class PictureFragment extends Fragment {
     private ImageButton mPhotoButton;
     private ImageView mPhotoView;
 
-    
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_activity, container, false);
+
+        mPhotoButton = v.findViewById(R.id.camera_button);
+        mPhotoView = v.findViewById(R.id.camera_photo);
+
+        return v;
+
+
+    }
 }
