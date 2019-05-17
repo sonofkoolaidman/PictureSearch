23be8f849f061f35fd69cbb5ac43cf78d33c654a
sonofkoolaidman <35243264+sonofkoolaidman@users.noreply.github.com>
2019-05-17 09:00:45 -0500

    Finished picture taking.

--- app/src/main/java/com/ctech/max/picturesearch/Picture.java
+++ app/src/main/java/com/ctech/max/picturesearch/Picture.java
@@ -1,8 +1,15 @@
 package com.ctech.max.picturesearch;
 
+import android.content.Context;
+
+import java.io.File;
 import java.util.UUID;
 
 public class Picture {
+
+    private Context mContext;
+
+    private static Picture sPicture;
 
     public UUID getId() {
         return mId;
@@ -12,5 +19,23 @@ public class Picture {
 
     public String getPictureFilename() {
         return "IMG_" + getId().toString() + ".jpg";
+    }
+
+    public static Picture get(Context context) {
+        if (sPicture == null) {
+            sPicture = new Picture(context);
+        }
+        return sPicture;
+    }
+
+    private Picture(Context context) {
+        // the context is where the database is stored while the app is running
+        mContext = context.getApplicationContext();
+    }
+
+    // get a link to the file's location
+    public File getPictureFile(Picture picture) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, picture.getPictureFilename());
     }
 }
--- app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
+++ app/src/main/java/com/ctech/max/picturesearch/PictureFragment.java
@@ -1,19 +1,33 @@
 package com.ctech.max.picturesearch;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.util.List;
+
 public class PictureFragment extends Fragment {
 
     private ImageButton mPhotoButton;
     private ImageView mPhotoView;
+    private File mPhotoFile;
+    private Picture mPicture;
+
+    //used to associate the camera app with the pictureFragment.
+    private static final int REQUEST_PHOTO = 2;
 
 
     @Nullable
@@ -23,10 +37,41 @@ public class PictureFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_activity, container, false);
 
         mPhotoButton = v.findViewById(R.id.camera_button);
+
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = FileProvider.getUriForFile(getActivity(),
+                        "com.ctech.max.criminalintent.fileprovider",
+                        mPhotoFile);
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+
+                for (ResolveInfo activity : cameraActivities) {
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,
+                            uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+
+                startActivityForResult(captureImage, REQUEST_PHOTO);
+            }
+        });
+
         mPhotoView = v.findViewById(R.id.camera_photo);
 
         return v;
 
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mPhotoFile = Picture.get(getActivity()).getPictureFile(mPicture);
     }
 }
